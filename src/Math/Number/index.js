964a3d38328fd1808da7107c84dba209b29faeb9
Max <superbialux@d3feat.website>
2022-05-01 22:30:54 +0400

    Add range mapping

--- src/Math/Number/index.js
+++ src/Math/Number/index.js
@@ -13,6 +13,12 @@ class Num {
   static normalize(axis, limit) {
     return axis / limit;
   }
+
+  static map(n, start1, stop1, start2, stop2) {
+    const newval =
+      ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+    return newval;
+  }
 }
 
 export default Num;
